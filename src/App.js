8b0f90373029dce8d533dc44ce24eebd2c8f63ec
Diego Lopes <diegolopes087@gmail.com>
2024-03-11 14:46:31 -0300

    Firebase initial configuration

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+import './App.css'
+import { initializeApp } from 'firebase/app'
+import { getAnalytics } from 'firebase/analytics'
+
+const firebaseConfig = initializeApp({
+  apiKey: 'AIzaSyAES0YYLs-K_fxgIPSAzM3jZEuKyvKQ8kY',
+  authDomain: 'realtimechat-83158.firebaseapp.com',
+  projectId: 'realtimechat-83158',
+  storageBucket: 'realtimechat-83158.appspot.com',
+  messagingSenderId: '360580154791',
+  appId: '1:360580154791:web:258e088cf0919a54a56282',
+  measurementId: 'G-SHSD9DZXFB',
+})
+
+const app = initializeApp(firebaseConfig)
+const analytics = getAnalytics(app)
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <header className='App-header'></header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
