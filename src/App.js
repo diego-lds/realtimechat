be81f469ab235809305b1f9fec5295ea8a625e24
Diego Lopes <diegolopes087@gmail.com>
2024-03-14 22:04:58 -0300

    Show messages on screen

--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
-import { initializeApp } from 'firebase/app'
+import './App.css'
+import { useEffect } from 'react'
+
 import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth'
 import { useAuthState } from 'react-firebase-hooks/auth'
-import './App.css'
+import { useCollectionData } from 'react-firebase-hooks/firestore'
 
-initializeApp({
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECTID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.REACT_APP_MEASURAMENT_ID,
-})
+import {
+  collection,
+  doc,
+  getDocs,
+  limit,
+  onSnapshot,
+  orderBy,
+  query,
+} from 'firebase/firestore'
+import { getFirestore } from 'firebase/firestore'
+import { config } from './firebaseConfig'
+import { initializeApp } from 'firebase/app'
 
+const app = initializeApp(config)
+const db = getFirestore(app)
 const auth = getAuth()
 
 function App() {
   const [user] = useAuthState(auth)
+
+  const getMessages = async () => {
+    const querySnapshot = await getDocs(collection(db, 'messages'))
+    querySnapshot.forEach(doc => {
+      console.log(doc.id, ' => ', doc.data())
+    })
+  }
+
+  useEffect(() => {
+    console.clear()
+  }, [])
+
   return (
     <div className='App'>
       <header className='App-header'>
@@ -25,6 +44,37 @@ function App() {
       </header>
       <section>{user ? <ChatRoom /> : <SignIn />}</section>
     </div>
+  )
+}
+
+function ChatRoom() {
+  const messagesRef = collection(db, 'messages')
+  const Query = query(messagesRef)
+  const [messages] = useCollectionData(Query, { id: 'id' })
+
+  return (
+    <ul>
+      {messages &&
+        messages.map(message => {
+          console.log(messages)
+          return (
+            <li>
+              <img alt='alt' src='./logo192.png' width='50px' />
+              <div className='message'>
+                <p>{message.text}</p>
+              </div>
+            </li>
+          )
+        })}
+    </ul>
+  )
+}
+
+function ChatMessage({ uid, text, photoUrl }) {
+  return (
+    <li>
+      <span>{photoUrl}</span>
+    </li>
   )
 }
 
@@ -53,8 +103,9 @@ function SignOut() {
   )
 }
 
-function ChatRoom() {
-  return <h1>CHAT</h1>
-}
-
 export default App
+// const listenMessages = async () => {
+//   const unsub = onSnapshot(doc(db, 'messages'), doc => {
+//     console.log('Current data: ', doc.data())
+//   })
+// }
