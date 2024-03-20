3158a203de468bcd75cad466b1ceaef14d6bc808
Diego Lopes <diegolopes087@gmail.com>
2024-03-20 15:27:33 -0300

    temporary disabling comunication

--- src/App.jsx
+++ src/App.jsx
@@ -37,22 +37,22 @@ function App() {
 
         const unsuscribe = onSnapshot(queryMessages, snapshot => {
             let messages = [];
-            snapshot.forEach(doc => {
-                const userIdMatch = doc.data().uid === user?.uid;
-                console.log(doc.data().uid, user?.uid);
-                messages.push({
-                    ...doc.data(),
-                    id: doc.id,
-                    key: doc.id,
-                    userIdMatch,
-                });
-            });
+            // snapshot.forEach(doc => {
+            //     const userIdMatch = doc.data().uid === user?.uid;
+            //     console.log(doc.data().uid, user?.uid);
+            //     messages.push({
+            //         ...doc.data(),
+            //         id: doc.id,
+            //         key: doc.id,
+            //         userIdMatch,
+            //     });
+            // });
             setMessages(messages);
         });
 
         return () => unsuscribe();
         // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+    }, [messages]);
 
     const sendMessage = async event => {
         event.preventDefault();
@@ -119,9 +119,7 @@ function App() {
 
 function SignIn() {
     const signInWithGoogle = () => {
-        signInWithPopup(auth, new GoogleAuthProvider())
-            .then(result => console.log(result))
-            .catch(error => console.error({ error }));
+        signInWithPopup(auth, new GoogleAuthProvider());
     };
 
     return (
@@ -129,7 +127,7 @@ function SignIn() {
             <button
                 type='button'
                 onClick={signInWithGoogle}
-                className='text-white bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 me-2 mb-2'
+                className='text-white  bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 me-2 mb-2'
             >
                 <svg
                     className='w-4 h-4 me-2'
@@ -138,9 +136,9 @@ function SignIn() {
                     viewBox='0 0 18 19'
                 >
                     <path
-                        fill-rule='evenodd'
+                        fillRule='evenodd'
                         d='M8.842 18.083a8.8 8.8 0 0 1-8.65-8.948 8.841 8.841 0 0 1 8.8-8.652h.153a8.464 8.464 0 0 1 5.7 2.257l-2.193 2.038A5.27 5.27 0 0 0 9.09 3.4a5.882 5.882 0 0 0-.2 11.76h.124a5.091 5.091 0 0 0 5.248-4.057L14.3 11H9V8h8.34c.066.543.095 1.09.088 1.636-.086 5.053-3.463 8.449-8.4 8.449l-.186-.002Z'
-                        clip-rule='evenodd'
+                        clipRule='evenodd'
                     />
                 </svg>
                 Entrar na sala com Google
--- src/components/ChatRoom.jsx
+++ src/components/ChatRoom.jsx
@@ -1,6 +1,6 @@
 const ChatRoom = ({ messages, reference }) => {
     return (
-        <ul className='flex flex-col max-h-full   mt-2 bg-stone-200 '>
+        <ul className='flex flex-col max-h-full mt-2 bg-stone-200 '>
             {messages?.length &&
                 messages.map(msg => {
                     return <ChatMessage {...msg} />;
@@ -10,24 +10,20 @@ const ChatRoom = ({ messages, reference }) => {
     );
 };
 
-function ChatMessage({ displayName, photoURL, text, id, uid, userIdMatch }) {
-    console.log(userIdMatch);
+function ChatMessage({ photoURL, text, id, userIdMatch }) {
     return (
         <li
             className={`flex p-2 ${userIdMatch ? 'flex-row-reverse' : ''}`}
             key={id}
         >
-            {/* <li className={'flex justify-end'} key={id}> */}
             <img
                 className='rounded-full m-2 self-start drop-shadow-md'
-                // className='rounded-full mr-3 self-start'
                 alt='user face'
                 src={photoURL || 'default-profile.svg'}
                 width={45}
             />
             <div className='rounded-xl  rounded-tr-none border-gray-200  text-justify  break-all drop-shadow-lg p-2 bg-white'>
                 <p className='text-md font-sans'>{text}</p>
-                {/* <p className='break-normal'>{text}</p> */}
             </div>
         </li>
     );
