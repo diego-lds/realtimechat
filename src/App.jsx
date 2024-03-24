30ccfeb19788aaebe7e1d18768be63d9043a6a7b
Diego Lopes <diegolopes087@gmail.com>
2024-03-23 21:10:40 -0300

    Center button

--- src/App.jsx
+++ src/App.jsx
@@ -121,7 +121,7 @@ function SignIn() {
             <button
                 type='button'
                 onClick={signInWithGoogle}
-                className='text-white ml-[40vw] mt-10 bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 me-2 mb-2'
+                className='text-white ml-[45vw] mt-[45vh]  bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 me-2 mb-2'
             >
                 <svg
                     className='w-4 h-4 me-2'
