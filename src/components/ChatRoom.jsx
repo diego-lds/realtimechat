e3440b73aeea7bfd3f6946cfa4b47a6724901227
Diego Lopes <diegolopes087@gmail.com>
2024-03-29 13:16:27 -0300

    refactor

--- src/components/ChatRoom.jsx
+++ src/components/ChatRoom.jsx
@@ -1,35 +1,33 @@
 const ChatRoom = ({ messages }) => {
     if (!messages.length) return;
+
     return (
         <ul className='flex flex-col bg-transparent '>
             {messages &&
-                messages.map(
-                    ({ photoURL, text, uid, displayName, sender, msgId }) => (
-                        <li
-                            className={`flex m-1 mensagem ${
-                                sender !== uid ? 'flex-row-reverse' : ''
-                            }`}
-                            key={msgId}
-                        >
-                            <img
-                                className='m-2 rounded-full self-start'
-                                alt='user face'
-                                src={photoURL || 'default-profile.svg'}
-                                width={50}
-                                height={50}
-                            />
-
-                            <div className=' rounded-xl p-1 max-w-screen-sm  border-gray-200 text-justify break-all drop-shadow-lg  bg-white'>
-                                <span>
-                                    <p className='p-1 text-indigo-900'>
-                                        {displayName}
-                                    </p>
-                                </span>
-                                <p className='text-sm p-1'>{text}</p>
-                            </div>
-                        </li>
-                    )
-                )}
+                messages.map(m => (
+                    <li
+                        className={`flex m-1 mensagem ${
+                            m.sender !== m.uid ? 'flex-row-reverse' : ''
+                        }`}
+                        key={m.msgId}
+                    >
+                        <img
+                            className='m-2 rounded-full self-start'
+                            alt='user face'
+                            src={m.photoURL || 'default-profile.svg'}
+                            width={50}
+                            height={50}
+                        />
+                        <div className=' rounded-xl p-1 max-w-screen-sm  border-gray-200 text-justify break-all drop-shadow-lg  bg-white'>
+                            <span>
+                                <p className='p-1 text-indigo-900'>
+                                    {m.displayName}
+                                </p>
+                            </span>
+                            <p className='text-sm p-1'>{m.text}</p>
+                        </div>
+                    </li>
+                ))}
         </ul>
     );
 };
