a00ff11d99469227e3924c17de31ba09e7994c2a
Diego Lopes <diegolopes087@gmail.com>
2024-10-28 19:47:37 -0300

    Mostrar users logados

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,24 +1,18 @@
-// Sidebar.js
 import { useEffect, useState } from "react";
-
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, doc, setDoc } from "firebase/firestore";
 import { db } from "../App";
 
-function Sidebar() {
+function Sidebar({ currentUserId, openChat }) {
   const [users, setUsers] = useState([]);
 
-  const handleOnClick = (id) => {
-    console.log(id);
-  };
   useEffect(() => {
     const fetchUsers = async () => {
       try {
         const usersCollection = collection(db, "users");
         const usersSnapshot = await getDocs(usersCollection);
-        const usersList = usersSnapshot.docs.map((doc) => ({
-          id: doc.id,
-          ...doc.data(),
-        }));
+        const usersList = usersSnapshot.docs
+          .map((doc) => ({ id: doc.id, ...doc.data() }))
+          .filter((user) => user.id !== currentUserId);
         setUsers(usersList);
       } catch (error) {
         console.error("Erro ao buscar usuários:", error);
@@ -26,18 +20,42 @@ function Sidebar() {
     };
 
     fetchUsers();
-  }, []);
-  console.log(users);
+  }, [currentUserId]);
+
+  const startChat = async (selectedUser) => {
+    const chatId = [currentUserId, selectedUser.id].sort().join("_");
+    const chatRef = doc(db, "chats", chatId);
+    await setDoc(
+      chatRef,
+      {
+        participants: [currentUserId, selectedUser.id],
+        createdAt: new Date(),
+      },
+      { merge: true }
+    );
+
+    openChat(chatId, selectedUser);
+  };
+
   return (
-    <aside className="w-64 bg-slate-500 text-white h-screen p-4 ">
+    <aside className="bg-gray-800 h-full text-white p-2 overflow-y-auto">
       <h2 className="text-lg font-semibold mb-4">Usuários</h2>
       <ul className="space-y-2">
         {users.map((user) => (
-          <li key={user.id} className="p-2 rounded hover:bg-slate-600">
-            <div>
-              <button onClick={() => handleOnClick(user.id)}>
-                {user.displayName}
-              </button>
+          <li
+            key={user.id}
+            onClick={() => startChat(user)}
+            className="p-1 bg-gray-700 rounded hover:bg-gray-600 cursor-pointer"
+          >
+            <div className="flex items-center gap-2">
+              <img
+                src={user.photoURL || "default-profile.svg"}
+                className="size-12 rounded-full"
+              />
+              <p className="hidden md:block">
+                {" "}
+                {user.displayName || "Nome não encontrado"}
+              </p>
             </div>
           </li>
         ))}
